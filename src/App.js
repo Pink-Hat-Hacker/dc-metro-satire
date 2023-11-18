0f9b76c57593777bccd0c2faef10776a410f40a1
Pink-Hat-Hacker <zyv@udel.edu>
2023-11-17 20:44:05 -0500

    styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import "./App.css";
 import MetroMap from "./components/MetroMap";
 
 function App() {
   return (
     <div className="App">
+      <div className="dcms-heading">
+        <h1 className="dcms-name">🚇 dc metro satire 🚇</h1>
+        <div className="dcms-icons">
+          <a
+            href="https://github.com/pink-hat-hacker/dc-metro-satire"
+            target="_blank"
+          >
+            <FaGithub size={40} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/zoe-yoyo-valladares/"
+            target="_blank"
+          >
+            <FaLinkedin size={40} />
+          </a>
+        </div>
+      </div>
       <MetroMap></MetroMap>
+      <div className="dcms-footer">
+        <a href="https://developer.wmata.com/">
+          <h3>Powered by: WMATA API</h3>
+        </a>
+      </div>
     </div>
   );
 }
